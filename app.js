1731cc3c52be879337e2f6b9d06e1d4fe5d71aa5
Nicolas Traczuk <ntraczuk92@gmail.com>
2023-02-15 11:06:32 -0300

    Nueva actualizacion

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const proyectos = document.querySelector('#proyectos');
 const titulo = document.querySelector('#titulo');
 
 
+titulo.addEventListener('click', ()=> {
+    if(proyectos.style.display === 'none') {
+        proyectos.style.display = 'block';
+    } else {
+        proyectos.style.display = 'none'
+    }
+})
+
 titulo.addEventListener('click', abrirProyectos);
 
 
@@ -16,23 +24,24 @@ function abrirProyectos() {
     anchor.target ='_blank'
     anchor.href = 'https://proyecto-cotizador-seguros.netlify.app/';
     anchor.style.color = 'rgba(0, 26, 255, 0.507)';
+    anchor.style.display = 'block'
     
     anchor2.textContent = 'Proyecto Presupuesto';
     anchor2.target ='_blank'
     anchor2.href = 'https://presupuesto-nicolas-traczuk.netlify.app/';
     anchor2.style.color = 'rgba(0, 26, 255, 0.507)';
-    
+    anchor2.style.display = 'block'
+
     anchor3.textContent = 'Proyecto Administrador de citas/pacientes';
     anchor3.target ='_blank'
     anchor3.href = 'https://citas-nicolas-traczuk.netlify.app/';
     anchor3.style.color = 'rgba(0, 26, 255, 0.507)';
-
+    anchor3.style.display = 'block'
 
     proyectos.appendChild(anchor);
     proyectos.appendChild(anchor2); 
     proyectos.appendChild(anchor3);
 
-    
 }
 
 
@@ -41,3 +50,4 @@ function limpiarHTML() {
         proyectos.removeChild(proyectos.firstChild)
     }
 }
+
